3a1c13518337558cc45a77f3c994dc3c95543955
Daniel Holý <Daniel.Holy@student.souepl.cz>
2024-11-18 09:30:47 +0100

    pole kosik nevim

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const nabidka = ["CPU", "GPU", "RAM"];
+
+function vypis(array, output){
+    for(let i =0; i<array.length; i++){
+        document.getElementById(output).innerHTML += i+1 + ". " + array[i] + "<br>";
+    }
+}
+
+function Add(){
+    const input = document.getElementById("add-component");
+    const content = document.getElementById("content");
+    const user_cart = document.getElementById("user-cart")
+
+    if(input == 1){
+        kosik.push(nabidka[0]);
+    }
+
+    else if(input.value == 2){
+        kosik.push(nabidka[1]);
+    }
+
+    else if(input.value == 3){
+        kosik.push(nabidka[2]);
+    }
+
+    else{
+        alert("Chyba")
+    }
+    vypis(kosik, "user-cart");
+}
+
+vypis(nabidka, "content")
